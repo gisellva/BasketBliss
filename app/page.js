8bb5c70ccb286e8dd30b0634117b5d26cda5c6b7
heidy vanegas <depgisell@gmail.com>
2023-07-14 23:15:45 -0500

    segundo

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <p>hola como estas amigo</p>
+      <p>esta es la rama principal main</p>
     </main>
   )
 }
