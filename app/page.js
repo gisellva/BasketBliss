60e4d5c5361f129a69c7c4e83c0d9ac5684ee197
heidy vanegas <depgisell@gmail.com>
2023-07-15 00:38:41 -0500

    creacion de los primeros componenetes y creacion de la primera carta

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import CardsComponet from './components/Carts/CardComponentHome'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <p>esta es la rama principal main</p>
+     <CardsComponet/>
     </main>
   )
 }
