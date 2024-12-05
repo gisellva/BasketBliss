28eded54a12d1cbc9655b1d2d4bffc0ddeeadc24
Gisellvanegas <productofficerads@fundacioncapital.org>
2024-12-05 16:06:57 -0500

    redireccionamiento a home

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/Home', // Página a la que quieres redirigir
+          permanent: true, // Marca la redirección como permanente (301)
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
